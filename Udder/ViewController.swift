444540e3653ebccf91563bb5effa95c06efec6b7
vishva <vishvathehulk@gmail.com>
2019-12-29 10:33:59 -0500

    first commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Vishva on 2019-12-29.
+//  Copyright © 2019 prismtek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
